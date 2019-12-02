f025a7ca0ed887ae013037caa179c064812a155e
ckastner12 <github email address>
2019-12-02 09:13:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1, string.length)
+  }
+
+  static sanitize(string) {
+    let splitted = string.split(" ")
+    for (let i = 0; i < splitted.length; i++) {
+      splitted[i] = splitted[i].replace(/[^A-Za-z0-9-']+/g, '')
+    }
+    return splitted.join(" ")
+  }
+  
+  static titleize(string) {
+    let splitted = string.split(" ")
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    splitted[0] = this.capitalize(splitted[0])
+    for (let i = 1; i < splitted.length; i++) {
+      if (!exceptions.includes(splitted[i])) {
+        splitted[i] = this.capitalize(splitted[i])
+      }
+    }
+    return splitted.join(" ")
+  }
 }
